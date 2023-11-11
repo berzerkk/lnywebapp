7fe7c95523fdd099634154d347e9c4af1de3a7a5
Xenok <xtoniox@hotmail.fr>
2023-11-11 02:25:24 +0100

    contact + 404

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+/* GET home page. */
+router.get('/terms', function(req, res, next) {
+  res.render('terms');
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact');
+});
+
+
+router.get('/404', function(req, res, next) {
+  res.render('404');
 });
 
 module.exports = router;
